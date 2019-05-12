373311dc0507bb2eac22ed3a316a14dd8a2f7c35
Vinicius Fernandes <viniciussfernandes12@gmail.com>
2019-05-12 16:09:53 -0300

    quem somos / partial interna produto

--- js/script.js
+++ js/script.js
@@ -9,3 +9,63 @@ $(document).ready(function () {
         }
     });
 });
+
+$(".custom-select2").each(function() {
+  var classes = $(this).attr("class"),
+      id      = $(this).attr("id"),
+      name    = $(this).attr("name");
+  var template =  '<div class="' + classes + '">';
+      template += '<span class="custom-select2-trigger">' + $(this).attr("name") + '</span>';
+      template += '<div class="custom-options">';
+      $(this).find("option").each(function() {
+        template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+      });
+  template += '</div></div>';
+  
+  $(this).wrap('<div class="custom-select2-wrapper"></div>');
+  $(this).hide();
+  $(this).after(template);
+});
+$(".custom-option:first-of-type").hover(function() {
+  $(this).parents(".custom-options").addClass("option-hover");
+}, function() {
+  $(this).parents(".custom-options").removeClass("option-hover");
+});
+$(".custom-select2-trigger").on("click", function() {
+  $('html').one('click',function() {
+    $(".custom-select2").removeClass("opened");
+  });
+  $(this).parents(".custom-select2").toggleClass("opened");
+  event.stopPropagation();
+});
+$(".custom-option").on("click", function() {
+  $(this).parents(".custom-select2-wrapper").find("select").val($(this).data("value"));
+  $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+  $(this).addClass("selection");
+  $(this).parents(".custom-select2").removeClass("opened");
+  $(this).parents(".custom-select2").find(".custom-select2-trigger").text( '' + $(this).text());
+});
+
+$('.slider_quem').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false
+});
+
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  prevArrow: '<div class="arrow feat-prev">&larr;</div>',
+        nextArrow: '<div class="arrow feat-next">&rarr;</div>',
+  dots: true,
+  arrows: true,
+  focusOnSelect: true
+});
