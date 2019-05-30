e7e775bb47b7202b489530c4c743c55af42b62c3
viniciusfernandes47 <viniciussfernandes12@gmail.com>
2019-05-30 19:13:15 -0300

    touchmove

--- js/script.js
+++ js/script.js
@@ -146,3 +146,9 @@ maxHeight = meuHeight + maxHeight;
 $(".dentro_produto h3").height(maxHeight);
 
 /* Fixar Scroll */
+document.addEventListener('touchmove', function(event) {
+  event = event.originalEvent || event;
+  if (event.scale !== 1) {
+     event.preventDefault();
+  }
+}, false);
